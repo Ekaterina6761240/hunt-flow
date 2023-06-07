311ce58c0564b1c15f10050d1d627066a5687089
Ekaterina <egm6761240@gmail.com>
2023-06-07 17:06:26 +0300

    initial

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,6 +15,12 @@ export default function Layout({ initState }) {
             __html: `window.initState=${JSON.stringify(initState)}`,
           }}
         />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
         <script defer src="/app.js" />
         <script defer src="/vendor.js" />
         <title>Document</title>
